870666df27eaf6b51a4760cf2146404ff92e5a74
pedro <pedro1713@gmail.com>
2016-09-14 10:39:43 -0500

    Added most of the code skeleton

--- buttonSubscriber.py
+++ buttonSubscriber.py
@@ -9,5 +9,15 @@ def on_connect(client, userdata, rc)
 # The callback for when a PUBLISH message is received from the broker
 def on_message(client, userdata, msg)
     topic=msg.topic
+    data = msg.payload
 
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("localhost", 1883, 60)
+
+#Blocking call that handles all network traffic and dispatches callbacks.
+#Also handles reconnection
+client.loop_forever()
 
